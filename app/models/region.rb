44a3f31227177f42582a0f693ab5a715fd4524c9
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-05-14 13:44:15 +0300

    correct path to views

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,5 @@
 class Region < ApplicationRecord
   belongs_to :country
   has_many :cities, dependent: :destroy
-  has_many :campings, through: :citiess, dependent: :destroy
+  has_many :campings, through: :cities, dependent: :destroy
 end
