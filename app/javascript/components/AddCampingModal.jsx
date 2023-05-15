a05b8f26fc15d4e1c440fa46b1e93b58e1590a80
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-05-15 16:09:59 +0300

    Add react Components

---
+++ app/javascript/components/AddCampingModal.jsx
@@ -0,0 +1,99 @@
+import { Button, Form, Input, Modal, Select } from "antd";
+import React from "react";
+
+const { Option } = Select;
+
+class AddCampingModal extends React.Component {
+  formRef = React.createRef();
+  state = {
+    visible: false,
+  };
+
+  onFinish = (values) => {
+    const url = "api/v1/beers/";
+    fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    })
+      .then((data) => {
+        if (data.ok) {
+          this.handleCancel();
+
+          return data.json();
+        }
+        throw new Error("Network error.");
+      })
+      .then(() => {
+        this.props.reloadCampings();
+      })
+      .catch((err) => console.error("Error: " + err));
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Button type="primary" onClick={this.showModal}>
+          Create New +
+        </Button>
+
+        <Modal title="Add New Camping ..." visible={this.state.visible} onCancel={this.handleCancel} footer={null}>
+          <Form ref={this.formRef} layout="vertical" onFinish={this.onFinish}>
+            <Form.Item name="name" label="Name" rules={[{ required: true, message: "Please input your camping name!" }]}>
+              <Input placeholder="Input your beer brand" />
+            </Form.Item>
+
+            <Form.Item name="body" label="Body" rules={[{ required: true, message: "Please input your camping body!" }]}>
+              <Input placeholder="Input your camping body" />
+            </Form.Item>
+
+            <Form.Item
+              name="country"
+              label="Country"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the country of the camping!",
+                },
+              ]}
+            >
+              <Select showSearch placeholder="Select your beer country" optionFilterProp="children" style={{ width: "100%" }}>
+                <Option value="Russia">Russia</Option>
+                <Option value="USA">USA</Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item name="region" label="Region" rules={[{ required: true, message: "Please input the region!" }]}>
+              <Input type="string" placeholder="Input name of the region" />
+            </Form.Item>
+            <Form.Item name="city" label="City" rules={[{ required: true, message: "Please input the city!" }]}>
+              <Input type="string" placeholder="Input name of the city" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default AddCampingModal;
--- app/javascript/components/Campings.jsx
+++ app/javascript/components/Campings.jsx
@@ -1,8 +1,57 @@
-state = {
+import { Table, message, Popconfirm } from "antd";
+import React from "react";
+import AddCampingModal from "./AddCampingModal";
+
+class Campings extends React.Component {
+  columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Body",
+      dataIndex: "body",
+      key: "body",
+    },
+    {
+      title: "Country",
+      dataIndex: "country",
+      key: "country",
+    },
+    {
+      title: "Region",
+      dataIndex: "region",
+      key: "region",
+    },
+    {
+        title: "City",
+        dataIndex: "city",
+        key: "city",
+      },
+    {
+      title: "",
+      key: "action",
+      render: (_text, record) => (
+        <Popconfirm title="Are you sure to delete this camping?" onConfirm={() => this.deleteCamping(record.id)} okText="Yes" cancelText="No">
+          <a href="#" type="danger">
+            Delete{" "}
+          </a>
+        </Popconfirm>
+      ),
+    },
+  ];
+
+  state = {
     campings: [],
   };
-  loadCampigss = () => {
-    const url = "controllers/v1/beers/index";
+
+  componentDidMount() {
+    this.loadCampings();
+  }
+
+  loadCampings = () => {
+    const url = "api/v1/beers/index";
     fetch(url)
       .then((data) => {
         if (data.ok) {
@@ -11,20 +60,55 @@ state = {
         throw new Error("Network error.");
       })
       .then((data) => {
-        data.forEach((beer) => {
+        data.forEach((camping) => {
           const newEl = {
-            key: beer.id,
-            id: beer.id,
-            brand: beer.brand,
-            style: beer.style,
-            country: beer.country,
-            quantity: beer.quantity,
+            key: camping.id,
+            id: camping.id,
+            name: camping.name,
+            body: camping.body,
+            country: camping.country,
+            region: camping.region,
+            city: camping.city,
           };
-  
+
           this.setState((prevState) => ({
-            beers: [...prevState.beers, newEl],
+            campings: [...prevState.campings, newEl],
           }));
         });
       })
       .catch((err) => message.error("Error: " + err));
   };
+
+  reloadCampings = () => {
+    this.setState({ campings: [] });
+    this.loadCampings();
+  };
+
+  deleteCampings = (id) => {
+    const url = `api/v1/beers/${id}`;
+
+    fetch(url, {
+      method: "delete",
+    })
+      .then((data) => {
+        if (data.ok) {
+          this.reloadCampings();
+          return data.json();
+        }
+        throw new Error("Network error.");
+      })
+      .catch((err) => message.error("Error: " + err));
+  };
+
+  render() {
+    return (
+      <>
+        <Table className="table-striped-rows" dataSource={this.state.campings} columns={this.columns} pagination={{ pageSize: 5 }} />
+
+        <AddCampingModal reloadCampings={this.reloadCampings} />
+      </>
+    );
+  }
+}
+
+export default Campings;
